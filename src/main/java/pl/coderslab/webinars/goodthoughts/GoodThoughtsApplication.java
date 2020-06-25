3417b280127307cd5899f8af46d175bdf970759d
honestit <michal.kupisinski@gmail.com>
2020-06-25 11:28:19 +0200

    Podstawowa struktura projektu

---
+++ src/main/java/pl/coderslab/webinars/goodthoughts/GoodThoughtsApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.webinars.goodthoughts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodThoughtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodThoughtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/coderslab/webinars/goodthoughts/GoodThoughtsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.webinars.goodthoughts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodThoughtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
